0932b7ac4f3843822b2024622de0f6e169ad90b0
jamesg-03 <dpnv0860@leeds.ac.uk>
2026-03-25 16:10:45 +0000

    mc

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -9,17 +9,39 @@
 #include <stdio.h>
 #include <string.h>
 
+// function to convert hexadecimal to decimal
 int main(void){
-	long decimal=0;
-	char hex[9];
-	
-	printf("Enter a hexadecimal:");
+    long decimal = 0;
+    char hex[9];
+    int i;
+    
+	// take user input and validate it using 8 bits
+    printf("Enter a hexadecimal number: ");
+    scanf("%8s", hex); //
 
-	// if input contains invalid hex digit
-	// printf("Error: Invalid Hexadecimal\n");
-	
-	// print the decimal result
-	printf("decimal:%ld\n", decimal);
-	
-	return 0;
+	// for loop to map each character
+    for (i = 0; hex[i] != '\0'; i++) {
+        int val;
+        char c = hex[i];
+
+        // converting characters to integers
+        if (c >= '0' && c <= '9') {
+            val = c - '0';
+        } else if (c >= 'a' && c <= 'f') {
+            val = c - 'a' + 10;
+        } else if (c >= 'A' && c <= 'F') {
+            val = c - 'A' + 10;
+        } else {
+            printf("Error: Invalid Hexadecimal\n");
+            return 1;
+        }
+
+		// final conversion
+        decimal = (decimal * 16) + val;
+    }
+    
+    // print result in decimal
+    printf("decimal:%ld\n", decimal);
+    
+    return 0;
 }
